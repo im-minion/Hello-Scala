cd450488684232b8eaf7dfaac02f09e74a530593
im-minion <vaibhavminiyar@gmail.com>
2021-07-21 17:12:55 +0530

    started with the import statement, static using java lang package

--- src/s_02/FrenchDate.scala
+++ src/s_02/FrenchDate.scala
@@ -1,6 +1,6 @@
 package s_02
 
-import java.text.DateFormat._ //{LONG, getDateInstance}
+import java.text.DateFormat._ //{LONG, getDateInstance} // _ means all import
 import java.util.{Date, Locale}
 
 object FrenchDate {
