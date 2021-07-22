851851eeafec3e8307e90a86406e04074b2b798c
im-minion <vaibhavminiyar@gmail.com>
2021-07-22 18:15:31 +0530

    added class

--- src/s_04_classes/ScalaClass.scala
+++ src/s_04_classes/ScalaClass.scala
@@ -8,4 +8,13 @@ class Complex(real: Double, imaginary: Double) {
   def re(): Double = real
 
   def im(): Double = imaginary
+  override def toString: String =
+    "" + re + (if (im >= 0) "+" else "") + im + "i"
+}
+
+object ComplexNumbers {
+  def main(args: Array[String]): Unit = {
+    val c = new Complex(1.2, 3.4)
+    println("Overridden toString(): " + c.toString)
+  }
 }
