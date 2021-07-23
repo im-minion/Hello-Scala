51d34095afd48783e24e44f3570746f776292b08
im-minion <vaibhavminiyar@gmail.com>
2021-07-23 18:41:21 +0530

    added class

--- src/s_04_classes/ScalaClass.scala
+++ src/s_04_classes/ScalaClass.scala
@@ -18,3 +18,16 @@ object ComplexNumbers {
     println("Overridden toString(): " + c.toString)
   }
 }
+
+// inheritance and overriding
+object ComplexNumbers2 {
+  def main(args: Array[String]): Unit = {
+    val c = new Complex(1.2, 3.4)
+    println("imaginary part: " + c.im())
+  }
+}
+
+class Complex2(real: Double, imaginary: Double) {
+  def re: Double = real
+  def im: Double = imaginary
+}
